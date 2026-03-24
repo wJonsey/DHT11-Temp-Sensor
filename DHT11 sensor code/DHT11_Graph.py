c617f16c0f3af81921d1e843674d7dfe3199b527
Will Jones <20188948@student.chelmsford.ac.uk>
2026-03-24 11:39:58 +0000

    updated formating for Timestamp

--- DHT11 sensor code/DHT11_Graph.py
+++ DHT11 sensor code/DHT11_Graph.py
@@ -1,17 +1,35 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
-data = pd.read_csv('DHT11_log.csv')
+df = pd.read_csv('DHT11_log.csv')
 
-x = data["Timestamp"] 
-y1 = data['avg_temp']
-y2 = data['avg_humidity']
+#30 minute conversion for timestamp every 30 minutes
+df["Timestamp"] = pd.to_datetime(df["Timestamp"])
+plt.gca().xaxis.set_major_locator(mdates.MinuteLocator(interval=30))
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
 
+
+
+#setting axis
+x = df["Timestamp"] 
+y1 = df['avg_temp']
+y2 = df['avg_humidity']
+
+
+#plotting data
 plt.plot (x,y1, label = 'Temperature'   )
 plt.plot (x,y2, label = 'Humidity'    )
+plt.ylabel('Temperature')
 plt.xlabel('Timestamp')
-plt.suptitle('DHT11 Sensor Data')
 
+
+
+
+#saving and doing layout
+plt.suptitle('DHT11 Sensor Data')
+plt.xticks(rotation=45)
+plt.tight_layout()
 plt.legend()
 plt.savefig('Dht11_graph.png')
 plt.show()
